b3606c76cc0ecb8e257d8568520a6c4132bbdd35
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-10-02 13:30:58 +0800

    only create new flight in db if not exist

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,24 +3,29 @@ require 'HTTParty'
 
 s = Rufus::Scheduler.singleton
 
-s.every '1m' do
+s.every '5s' do
     url = 'https://flifo-qa.api.aero/flifo/v3/flights/sin/d'
     headers = {'X-apiKey' => '2cfd0827f82ceaccae7882938b4b1627', 'Accept' => 'application/json'}
     response = HTTParty.get(url, headers: headers) 
     response['flightRecord'].each do |record|
         Rails.logger.info "Hello, it's #{response}"
-        new_flight = Flight.new({
-            :airlineCode => record["operatingCarrier"]["airlineCode"],
-            :number => record["operatingCarrier"]["flightNumber"],
-            :scheduled => record["scheduled"],
-            :status => record['status'],
-            :aircraft => record['aircraft'],
-            :city => record['city'],
-            :airportCode => record['airportCode'],
-            :terminal => record['terminal'],
-            :adi => 'd'
-        })
-        new_flight.save()
+        airlineCode = record["operatingCarrier"]["airlineCode"]
+        flightNumber = record["operatingCarrier"]["flightNumber"]
+        existing_flight = Flight.where(airlineCode: airlineCode, number: flightNumber)
+        if existing_flight.nil?
+            new_flight = Flight.new({
+                :airlineCode => airlineCode,
+                :number => flightNumber,
+                :scheduled => record["scheduled"],
+                :status => record['status'],
+                :aircraft => record['aircraft'],
+                :city => record['city'],
+                :airportCode => record['airportCode'],
+                :terminal => record['terminal'],
+                :adi => 'd'
+            })
+            new_flight.save()
+        end
     end
     Rails.logger.info "Hello, it's #{Time.now}"
     Rails.logger.flush
