508c5c8b72502df9964a5fa3fb92911afb08b457
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-10-02 12:34:22 +0800

    add airline code to flight

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001142852) do
+ActiveRecord::Schema.define(version: 20151002043357) do
 
   create_table "chat_boxes", force: :cascade do |t|
     t.datetime "open_time"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151001142852) do
     t.string   "airportCode"
     t.string   "terminal"
     t.string   "adi"
+    t.string   "airlineCode"
   end
 
   create_table "users", force: :cascade do |t|
