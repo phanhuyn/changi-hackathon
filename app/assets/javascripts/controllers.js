f70eca552c8ac3495d2459100fd6911e023b1033
Phuc Nguyen <phuc10cdt2@gmail.com>
2015-10-03 16:24:46 +0800

    Added user reg and comment

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,24 +1,39 @@
 (function() {
     angular.module('changi').controller('HomeController', ['$scope', 'Flight', 'ChatService', 'ChatBox', function ($scope, Flight, ChatService, ChatBox){
-        console.log("FAWEFAE");
         $scope.inputFlight = function(){
             Flight.query({flight_number: $scope.flightNumber}, function(flight){
                 $scope.flight = flight[0];
                 ChatService.setFlightId($scope.flight.id);
-                console.log(ChatService.getFlightId());
             });
         };
     }]);
-    angular.module('changi').controller('ChatBoxController', ['$scope', 'ChatService', 'ChatBox', function($scope, ChatService, ChatBox){
+    angular.module('changi').controller('ChatBoxController', ['$scope', 'ChatService', 'ChatBox', 'Comment', 'User', 
+        function($scope, ChatService, ChatBox, Comment, User){
+        $scope.user = null;
         $scope.$watch(function(){return ChatService.getFlightId();}, function(flight_id){
-            console.log(flight_id);
             if(flight_id){
                 ChatBox.query({flight_id: ChatService.getFlightId()}, function(chatbox){
                     $scope.chatbox = chatbox[0];
                     $scope.comments = $scope.chatbox.comments;
-                    console.log($scope.comments);
                 });
             }
         }, true);
+        $scope.addComment = function() {
+            Comment.save({user_id: $scope.user.id, content: $scope.newComment, chat_box_id: $scope.chatbox.id}, function(res){
+                console.log("Comment added");
+                console.log(res.toJSON());
+                $scope.comments.push(res.toJSON());
+            })
+        }
+        if(!$scope.user){
+            //call this block if user not exist
+            // $scope.user = {name: 'David', id: 1}
+        }
+        $scope.addUser = function(){
+            User.save({name: $scope.username}, function(res){
+                $scope.user = res;
+                console.log("User added");
+            });
+        }
     }]);
 })();
