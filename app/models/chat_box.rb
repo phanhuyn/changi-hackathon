eee1f5ffdf1f81dd1b2480dc9c20ce09a026716f
Phuc Nguyen <phuc10cdt2@gmail.com>
2015-10-03 13:41:15 +0800

    Added default time for chatbox

--- app/models/chat_box.rb
+++ app/models/chat_box.rb
@@ -1,4 +1,9 @@
 class ChatBox < ActiveRecord::Base
   belongs_to :flight, required: true
   has_many :comments
+  before_create :set_time_to_now
+  def set_time_to_now
+    self.open_time=Time.now
+    self.close_time=Time.now
+  end
 end
