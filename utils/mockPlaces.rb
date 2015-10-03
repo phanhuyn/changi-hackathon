87099f518487794c1d9750b2b7b1749677acbbf1
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-10-03 15:44:34 +0800

    added mock places creating script

---
+++ utils/mockPlaces.rb
@@ -0,0 +1,5 @@
+Place.create(:name => "Starbucks",
+             :tel => "911",
+             :address => "Departure / Check-in Hall South, #026-049 Terminal 2 Singapore Changi Airport, Singapore 819643",
+             :longitude => 2,
+             :latitude => 3)
