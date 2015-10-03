713b09ee8b5a3e56b749a6ccd44786c6add53c81
Phuc Nguyen <phuc10cdt2@gmail.com>
2015-10-03 15:10:12 +0800

    Added Chat box controller

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,10 +1,24 @@
 (function() {
-    angular.module('changi').controller('MainCtrl', ['$scope', 'Flight', function ($scope, Flight){
+    angular.module('changi').controller('HomeController', ['$scope', 'Flight', 'ChatService', 'ChatBox', function ($scope, Flight, ChatService, ChatBox){
+        console.log("FAWEFAE");
         $scope.inputFlight = function(){
             Flight.query({flight_number: $scope.flightNumber}, function(flight){
-                $scope.flight = flight;
+                $scope.flight = flight[0];
+                ChatService.setFlightId($scope.flight.id);
+                console.log(ChatService.getFlightId());
             });
         };
-        
+    }]);
+    angular.module('changi').controller('ChatBoxController', ['$scope', 'ChatService', 'ChatBox', function($scope, ChatService, ChatBox){
+        $scope.$watch(function(){return ChatService.getFlightId();}, function(flight_id){
+            console.log(flight_id);
+            if(flight_id){
+                ChatBox.query({flight_id: ChatService.getFlightId()}, function(chatbox){
+                    $scope.chatbox = chatbox[0];
+                    $scope.comments = $scope.chatbox.comments;
+                    console.log($scope.comments);
+                });
+            }
+        }, true);
     }]);
 })();
--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,5 +1,19 @@
 (function() {
     angular.module('changi').factory('Flight', function($resource){
-        return $resource('/flights');
+        return $resource('/flights.json');
+    });
+    angular.module('changi').factory('ChatBox', function($resource){
+        return $resource('/chat_boxes.json');
+    })
+    angular.module('changi').factory('ChatService', function(){
+        var flight_id = null;
+        return {
+            setFlightId: function(id) {
+                flight_id = id;
+            },
+            getFlightId: function() {
+                return flight_id;
+            }
+        }
     });
 })();
